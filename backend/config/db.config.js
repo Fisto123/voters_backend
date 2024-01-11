5a9a1c4f86d4c3be706f35eeda8a39e1cf8b800e
fisto123 <oluwafisayoiyiola@@gmail.com>
2024-01-11 15:59:10 +0100

    Update .gitignore to ignore config/

--- backend/config/db.config.js
+++
@@ -1,8 +0,0 @@
-export const dbConn = {
-  HOST: "127.0.0.1",
-  USER: "root",
-  PASSWORD: "Fistobobo12@",
-  DB: "voterz",
-  dialect: "mysql",
-  pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
-};
